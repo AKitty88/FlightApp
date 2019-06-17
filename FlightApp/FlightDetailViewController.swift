ec5233d7b03bbe5c9895707c566be362261887f1
KRECSIK <KRECSIK@desider-kirschners-imac.local>
2019-06-17 19:35:54 +1000

    flightTime is shown correctly on the FlightDetailView

--- FlightApp/FlightDetailViewController.swift
+++ FlightApp/FlightDetailViewController.swift
@@ -28,14 +28,29 @@ class FlightDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var dateString = flightDetail?.departure_date
+        let timeFormatter = DateFormatter()
+        //set the date format to dateOriginal's date format
+        timeFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss'.000'"
+        timeFormatter.locale = Locale(identifier: "en_US_POSIX") // set locale to reliable US_POSIX
+        let flightTime = timeFormatter.date(from: dateString!)
+        
+        //set the date format to the type of output that is needed
+        timeFormatter.dateFormat = "HH:mm a"
+        // Convert date to string
+        var flightTimeString = timeFormatter.string(from: flightTime ?? Date())
+        
+        // startDate.text = flightDateString
+        startTime.text = flightTimeString
+        
+        
         cityFrom.text = flightDetail?.departure_airport ?? "--"
         cityFromLong.text = flightDetail?.departure_city ?? "--"
-        startDate.text = flightDetail?.departure_date ?? "--"
-        startTime.text = flightDetail?.departure_date ?? "--"
         cityTo.text = flightDetail?.arrival_airport ?? "--"
         cityToLong.text = flightDetail?.arrival_city ?? "--"
-        arrivalDate.text = flightDetail?.arrival_date ?? "--"
-        arrivalTime.text = flightDetail?.arrival_date ?? "--"
+        // arrivalDate.text = flightDateString
+        //arrivalTime.text = flightTimeString
         
         flightNr.text = flightDetail?.flight_number ?? "--"
         terminal.text = flightDetail?.departure_airport ?? "--"
